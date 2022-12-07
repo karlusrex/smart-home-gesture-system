0dc84de3162186cb5be81a7896e388819be5a851
itzhapp <90254802+karlusrex@users.noreply.github.com>
2022-12-07 14:52:02 +0100

    Adding confirm cancel button to the Add/Edit/Remove activity

--- Android/app/src/main/java/com/smarthome/gesturecontrol/EditGesture.java
+++ Android/app/src/main/java/com/smarthome/gesturecontrol/EditGesture.java
@@ -1,13 +1,34 @@
 package com.smarthome.gesturecontrol;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class EditGesture extends AppCompatActivity {
 
+    Button buttonCancel;
+    Button buttonConfirm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_gesture);
+        setContentView(R.layout.activity_add_gesture);
+
+        buttonCancel = (Button) findViewById(R.id.cancel);
+        buttonConfirm = (Button) findViewById(R.id.confirm);
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGestureActivity(MainActivity.class);
+            }
+        });
+    }
+
+    public void openGestureActivity(Class gesture) {
+        Intent intent = new Intent(this, gesture);
+        startActivity(intent);
     }
 }
