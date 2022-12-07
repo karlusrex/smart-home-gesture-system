54dfab5bf31bd407c4df6411a55b8ecd74ac2a2a
itzhapp <90254802+karlusrex@users.noreply.github.com>
2022-12-07 13:53:59 +0100

    Adding Empty Activities for each button action
    Adding AddGesture, EditGesture, RemoveGesture activity

---
+++ Android/app/src/main/java/com/smarthome/gesturecontrol/AddGesture.java
@@ -0,0 +1,13 @@
+package com.smarthome.gesturecontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddGesture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_gesture);
+    }
+}
---
+++ Android/app/src/main/java/com/smarthome/gesturecontrol/RemoveGesture.java
@@ -0,0 +1,13 @@
+package com.smarthome.gesturecontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemoveGesture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remove_gesture);
+    }
+}
