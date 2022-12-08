11cfe3adbf7f3c1951dffdd27298e136020d9503
UglyWallach <118812603+UglyWallach@users.noreply.github.com>
2022-12-08 15:12:58 +0100

    gestureTracker

---
+++ Python/gestureTracker.py
@@ -0,0 +1,40 @@
+# Import the necessary Packages for this software to run
+import mediapipe
+import cv2
+from datetime import datetime, timedelta
+
+drawingModule = mediapipe.solutions.drawing_utils
+handsModule = mediapipe.solutions.hands
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+
+with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7,
+                       max_num_hands=1) as hands:
+    while True:
+        ret, frame = cap.read()
+
+        frame1 = cv2.resize(frame, (640, 480))
+
+        results = hands.process(cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB))
+        now = datetime.now()
+        pause = datetime.now()
+
+        if results.multi_hand_landmarks != None:
+            for handLandmarks in results.multi_hand_landmarks:
+                drawingModule.draw_landmarks(frame1, handLandmarks, handsModule.HAND_CONNECTIONS)
+
+                for i in [8, 12, 16, 20]:
+                    # p för att printa ut vilka fingrar som hålls upp
+                    if key == ord("p"):
+                        finger = handLandmarks.landmark[i].y
+                        reference = handLandmarks.landmark[i - 2].y
+                        fingerName = handsModule.HandLandmark(i).name
+                        if finger < reference:
+                            print(fingerName + "\n" + "True" + "\n")
+
+        cv2.imshow("Frame", frame1);
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):
+            break
