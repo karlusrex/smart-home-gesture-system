dd221a6da955ef48cd913530af97c0613d9b8e4a
itzhapp <90254802+karlusrex@users.noreply.github.com>
2022-12-11 12:34:26 +0100

    Cancel in AddGestureToThing activity will return to AddGesture activity

--- Android/app/src/main/java/com/smarthome/gesturecontrol/AddGestureToThing.java
+++ Android/app/src/main/java/com/smarthome/gesturecontrol/AddGestureToThing.java
@@ -24,7 +24,7 @@ public class AddGestureToThing extends AppCompatActivity {
         buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openGestureActivity(AddGestureToThing.class);
+                openGestureActivity(AddGesture.class);
             }
         });
 
