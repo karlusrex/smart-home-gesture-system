3030ce565ebb5af0ce1c5f52d7e6107d9267cd9a
itzhapp <90254802+karlusrex@users.noreply.github.com>
2022-12-12 11:11:13 +0100

    Create RunSSH for handling SSH connection
    Using Ganymed SSH-2 to handle SSH connection to the raspberry pi. This will be used to read terminal output on the raspberry pi to then display data in the app. It has a run command, planning on adding arguments into the python code to provide information back to the raspberry pi

---
+++ Android/app/src/main/java/com/smarthome/gesturecontrol/RunSSH.java
@@ -0,0 +1,46 @@
+package com.smarthome.gesturecontrol;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class RunSSH {
+
+    public void run (String command) {
+        String hostname = "hostname";
+        String username = "username";
+        String password = "password";
+        try
+        {
+            Connection conn = new Connection(hostname); //init connection
+            conn.connect(); //start connection to the hostname
+            boolean isAuthenticated = conn.authenticateWithPassword(username,
+                    password);
+            if (isAuthenticated == false)
+                throw new IOException("Authentication failed.");
+            Session sess = conn.openSession();
+            sess.execCommand(command);
+            InputStream stdout = new StreamGobbler(sess.getStdout());
+            BufferedReader br = new BufferedReader(new InputStreamReader(stdout));
+            //reads text
+            while (true){
+                String line = br.readLine(); // read line
+                if (line == null)
+                    break;
+                System.out.println(line);
+            }
+            /* Show exit status, if available (otherwise "null") */
+            System.out.println("ExitCode: " + sess.getExitStatus());
+            sess.close(); // Close this session
+            conn.close();
+        }
+        catch (IOException e)
+        { e.printStackTrace(System.err);
+            System.exit(2); }
+    }
+}
