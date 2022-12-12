4502989b89a2db112b03828bc76a8b16622eeb80
itzhapp <90254802+karlusrex@users.noreply.github.com>
2022-12-12 13:01:16 +0100

    Update gestureTracker.py adding gesture representation array
    Adding a gestureBoolArray that represents if a finger is up or down representing [Index_Finger_Tip, Middle_Finger_Tip, Ring_Finger_Tip, Pinky_Tip].
    
    Adding a time delay to make the app less performance heavy for the rapsberry pi when processing the video feed.
    
    Adding getGestureBoolArray for retreiving the data for dataHandler to be able to save as boolArray

--- Python/gestureTracker.py
+++ Python/gestureTracker.py
@@ -6,35 +6,44 @@ from datetime import datetime, timedelta
 drawingModule = mediapipe.solutions.drawing_utils
 handsModule = mediapipe.solutions.hands
 
+gestureBoolArray = [] #Contains 4 boolean values representing if finger is up or down. Representing [Index_Finger_Tip, Middle_Finger_Tip, Ring_Finger_Tip, Pinky_Tip]
+
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
 with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7,
                        max_num_hands=1) as hands:
+    pause = datetime.now() + timedelta(seconds=2.0)
+    
     while True:
         ret, frame = cap.read()
 
         frame1 = cv2.resize(frame, (640, 480))
 
         results = hands.process(cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB))
-        now = datetime.now()
-        pause = datetime.now()
 
-        if results.multi_hand_landmarks != None:
+        if results.multi_hand_landmarks != None and datetime.now().second >= pause.second:
             for handLandmarks in results.multi_hand_landmarks:
                 drawingModule.draw_landmarks(frame1, handLandmarks, handsModule.HAND_CONNECTIONS)
 
                 for i in [8, 12, 16, 20]:
-                    # p för att printa ut vilka fingrar som hålls upp
-                    if key == ord("p"):
-                        finger = handLandmarks.landmark[i].y
-                        reference = handLandmarks.landmark[i - 2].y
-                        fingerName = handsModule.HandLandmark(i).name
-                        if finger < reference:
-                            print(fingerName + "\n" + "True" + "\n")
+                    finger = handLandmarks.landmark[i].y
+                    reference = handLandmarks.landmark[i - 2].y
+                    fingerName = handsModule.HandLandmark(i).name
+                    if finger < reference:
+                        gestureBoolArray.append(1)
+                    else: 
+                        gestureBoolArray.append(0)
+                print(gestureBoolArray)
+                gestureBoolArray = []
+        
+            pause = datetime.now() + timedelta(seconds=2.0)
 
         cv2.imshow("Frame", frame1);
         key = cv2.waitKey(1) & 0xFF
 
         if key == ord("q"):
             break
+
+def getGestureBoolArray():
+    return gestureBoolArray
