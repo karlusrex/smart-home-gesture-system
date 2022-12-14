be326c22e14e5622cbbe29c48134433b4c0e2d69
itzhapp <90254802+karlusrex@users.noreply.github.com>
2022-12-14 12:29:42 +0100

    Update dataHandler to handle removing of Gesture
    Adding removeGesture which deletes the gesture with the name given by the user.
    
    Updating saveGesture to handle the deletion of a gesture, so that it does not add multiple of the same gesture by adding into empty indexing key value.

--- Python/dataHandler.py
+++ Python/dataHandler.py
@@ -8,11 +8,48 @@ def saveGesture(boolarray, name, action, device):
     gestures = getGestures()
     #Conflicting is true if you want to save the same gesture
     conflict = False
+    savedslots = []
     for savedgesture in gestures:
-        if boolarray == gestures[savedgesture]:
+        #Get all the keys in the data.json file
+        savedslots = gestures.keys()
+        #Checks if the boolarray is saved, meaning the gesture is taken
+        if boolarray == gestures[savedgesture]["array"]:
             conflict = True
+    
+    #Converts string values to integers
+    intsavedslots = [int(stringint) for stringint in savedslots]
+    #Sort the keys to be able to find if there is a slot missing
+    intsavedslots = sorted(intsavedslots)
 
+    empty = 0
+    #Checks if there is missing slot by counting from 1 to len of gestures
+    for n in range(1,len(gestures)):
+        if n not in intsavedslots:
+            #Found empty slot
+            empty = n
+            break
+    
     if not conflict:
-        gestures[len(gestures) + 1] = {"array":boolarray, "name":name, "action":action, "device":device}
+        #Default index
+        index = 0
+        if empty > 0:
+            index = empty
+        else:
+            index = len(gestures) + 1
+        gestures[index] = {"array":boolarray, "name":name, "action":action, "device":device}
         with open('./smart-home-gesture-system/Python/data/data.json', "w") as file:
+            json.dump(gestures, file)
+
+def editGesture():
+    pass
+
+def removeGesture(name):
+    gestures = getGestures()
+    removegesture = 0
+    for gesture in gestures:
+        if gestures[gesture]["name"] == name:
+            removegesture = gesture
+
+    gestures.pop(removegesture)
+    with open('./smart-home-gesture-system/Python/data/data.json', "w") as file:
             json.dump(gestures, file)
