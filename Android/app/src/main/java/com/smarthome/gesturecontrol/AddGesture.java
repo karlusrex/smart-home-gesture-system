072b9d8d18c687c97d1cece0d7ff7c83506af7ca
itzhapp <90254802+karlusrex@users.noreply.github.com>
2022-12-20 18:25:51 +0100

    Update AddGesture.java to display available sensors, and devices
    Adding AsyncTask to send a SSH request to the raspberry pi and retrieve data from the available devices, and sensors. Currently not set with any command as the path in raspberry pi might change in future.
    
    deviceList, and sensorList are used to append the values retrieved in the returnValueDevices/Sensors as the devices variable only passes as a reference.

--- Android/app/src/main/java/com/smarthome/gesturecontrol/AddGesture.java
+++ Android/app/src/main/java/com/smarthome/gesturecontrol/AddGesture.java
@@ -1,21 +1,27 @@
 package com.smarthome.gesturecontrol;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class AddGesture extends AppCompatActivity {
 
     Button buttonCancel;
     Button buttonConfirm;
 
-    ListView simpleListView;
-
+    String[] devices={};
+    String[] sensors={};
+    ArrayList<String> deviceList = new ArrayList<String>();
+    ArrayList<String> sensorList = new ArrayList<String>();
+    String returnValueDevices = "";
+    String returnValueSensors = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,35 @@ public class AddGesture extends AppCompatActivity {
 
         buttonCancel = (Button) findViewById(R.id.cancel);
         buttonConfirm = (Button) findViewById(R.id.confirm);
+        new AsyncTask<Integer, Void, Void>(){
+
+            @Override
+            protected Void doInBackground(Integer... params) {
+                //getting all the devices available, should be run as followed "python /location/to/python.py"
+                returnValueDevices = RunSSH.run("");
+                return null;
+            }
+            @Override
+            protected void onPostExecute(Void v) {
+                devices = returnValueDevices.split(" ");
+                for (String s : devices) {deviceList.add(s) ;}
+            }
+        }.execute(1);
+
+        new AsyncTask<Integer, Void, Void>(){
+
+            @Override
+            protected Void doInBackground(Integer... params) {
+                //getting all the sensors available, should be run as followed "python /location/to/python.py"
+                returnValueSensors = RunSSH.run("");
+                return null;
+            }
+            @Override
+            protected void onPostExecute(Void v) {
+                sensors = returnValueSensors.split(" ");
+                for (String s : sensors) {sensorList.add(s) ;}
+            }
+        }.execute(1);
 
         buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,11 +73,11 @@ public class AddGesture extends AppCompatActivity {
             }
         });
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.dropdown_item, getResources().getStringArray(R.array.devices));
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.dropdown_item, deviceList);
         AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
         autoCompleteTextView.setAdapter(arrayAdapter);
 
-        ArrayAdapter arrayAdapter2 = new ArrayAdapter(this, R.layout.dropdown_item, getResources().getStringArray(R.array.sensors));
+        ArrayAdapter arrayAdapter2 = new ArrayAdapter(this, R.layout.dropdown_item, SensorList);
         AutoCompleteTextView autoCompleteTextView2 = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView2);
         autoCompleteTextView2.setAdapter(arrayAdapter2);
 
