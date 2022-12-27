7ed4a13173ee034f9eb4406b9b3bfa2993cacc6d
itzhapp <90254802+karlusrex@users.noreply.github.com>
2022-12-27 14:44:56 +0100

    Add main.py to handle command line arguments from Raspberry Pi
    Usage is python main.py --function getDevices to get devices for example.
    Added command line support for getDevice, getSensors, saveGesture, editGestures, removeGesture. Also creating parsers for boolarray
    
    Did try implementing a privacy command line to toggle a boolean in gestureTracker.py but the value did not update. Possibly if creating multiprocessing or threading to handle this.

---
+++ Python/main.py
@@ -0,0 +1,35 @@
+import argparse
+import dataHandler
+import tellstickHandler
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--function", type=str, help="Name of the function to call")
+    parser.add_argument("--args", type=str, nargs="*", help="Arguments to pass to the function")
+    args = parser.parse_args()
+
+    if args.function == "getDevices":
+        devices = tellstickHandler.getDevices()
+        print(devices)
+    elif args.function == "getSensors":
+        sensors = tellstickHandler.getSensors()
+        print(sensors)
+    elif args.function == "saveGesture":
+        boolarray = [int(x) for x in args.args[0].strip('[]').split(',')]
+        name = args.args[1]
+        action = args.args[2]
+        device = int(args.args[3])
+        dataHandler.saveGesture(boolarray, name, action, device)
+    elif args.function == "editGesture":
+        oldname = args.args[0]
+        boolarray = [int(x) for x in args.args[1].strip('[]').split(',')]
+        newname = args.args[2]
+        action = args.args[3]
+        device = int(args.args[4])
+        dataHandler.editGesture(oldname, boolarray, newname, action, device)
+    elif args.function == "removeGesture":
+        name = args.args[0]
+        dataHandler.removeGesture(name)
+        
+if __name__ == "__main__":
+    main()
