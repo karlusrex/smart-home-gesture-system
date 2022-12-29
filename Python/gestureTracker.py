00c49bcc45c9d46f6b1a5b95ab22dee6d643145f
itzhapp <90254802+karlusrex@users.noreply.github.com>
2022-12-29 16:27:31 +0100

    Update gestureTracker.py to handle command line arguments
    Adding two new functions to gestureTracker.py, one setGestureBoolArray that takes a gesture bool array and writes to the file gesture_bool_array.txt in data folder.
    
    This function is linked to the getGestureBoolArray function that reads from the gesture_bool_array.txt in the data folder and returns the last known boolarray.
    
    This is then integrated into the main.py file to have a --start command line argument to start capturing video feed. To be able to get the gesture bool array it needs to be saved in the gesture_bool_array.txt

--- Python/gestureTracker.py
+++ Python/gestureTracker.py
@@ -4,49 +4,50 @@ import cv2
 from datetime import datetime, timedelta
 from dataHandler import handleGestureRecognition
 
-drawingModule = mediapipe.solutions.drawing_utils
-handsModule = mediapipe.solutions.hands
-
 gestureBoolArray = [] #Contains 4 boolean values representing if finger is up or down. Representing [Index_Finger_Tip, Middle_Finger_Tip, Ring_Finger_Tip, Pinky_Tip]
+#set is for windows / export for mac and linux
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-
-with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7,
-                       max_num_hands=1) as hands:
-    pause = datetime.now() + timedelta(seconds=2.0)
-    
-    while True:
-        ret, frame = cap.read()
-
-        frame1 = cv2.resize(frame, (640, 480))
-
-        results = hands.process(cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB))
-
-        if results.multi_hand_landmarks != None and datetime.now().second >= pause.second:
-            for handLandmarks in results.multi_hand_landmarks:
-                drawingModule.draw_landmarks(frame1, handLandmarks, handsModule.HAND_CONNECTIONS)
-
-                for i in [8, 12, 16, 20]:
-                    finger = handLandmarks.landmark[i].y
-                    reference = handLandmarks.landmark[i - 2].y
-                    fingerName = handsModule.HandLandmark(i).name
-                    if finger < reference:
-                        gestureBoolArray.append(1)
-                    else: 
-                        gestureBoolArray.append(0)
-                print(gestureBoolArray)
-                handleGestureRecognition(gestureBoolArray)
-
-                gestureBoolArray = []
-        
-            pause = datetime.now() + timedelta(seconds=2.0)
-
-        cv2.imshow("Frame", frame1);
-        key = cv2.waitKey(1) & 0xFF
-
-        if key == ord("q"):
-            break
+def setGestureBoolArray(gesture_bool_array):
+    with open('./data/gesture_bool_array.txt', 'w') as file:
+        file.write(str(gesture_bool_array))
 
 def getGestureBoolArray():
-    return gestureBoolArray
+    try:
+        with open('./data/gesture_bool_array.txt', 'r') as file:
+            gesture_bool_array = file.readline()
+    except IOError:
+        print("Error could not find gesture_bool_array.txt")
+
+    return gesture_bool_array
+
+def startGestureTracker():
+    global gestureBoolArray
+    handsModule = mediapipe.solutions.hands
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    pause = datetime.now() + timedelta(seconds=1.0)
+    with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7,
+                           max_num_hands=1) as hands:
+        while True:
+            ret, frame = cap.read()
+            frame1 = cv2.resize(frame, (640, 480))
+            if datetime.now().second >= pause.second:
+                results = hands.process(cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB))
+                if results.multi_hand_landmarks != None:
+                    for handLandmarks in results.multi_hand_landmarks:
+                        for i in [8, 12, 16, 20]:
+                            finger = handLandmarks.landmark[i].y
+                            reference = handLandmarks.landmark[i - 2].y
+                            if finger < reference:
+                                gestureBoolArray.append(1)
+                            else: 
+                                gestureBoolArray.append(0)
+                        print(gestureBoolArray)
+                        setGestureBoolArray(gestureBoolArray)
+                        handleGestureRecognition(gestureBoolArray)
+                        gestureBoolArray = []
+                pause = datetime.now() + timedelta(seconds=1.0)
+            cv2.imshow("Frame", frame1);
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
--- Python/main.py
+++ Python/main.py
@@ -1,19 +1,26 @@
 import argparse
 import dataHandler
 import tellstickHandler
+import gestureTracker
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--function", type=str, help="Name of the function to call")
     parser.add_argument("--args", type=str, nargs="*", help="Arguments to pass to the function")
+    parser.add_argument("--start", help="Starts the gesture tracker", action="store_true")
     args = parser.parse_args()
 
+    if args.start:
+        gestureTracker.startGestureTracker()
     if args.function == "getDevices":
         devices = tellstickHandler.getDevices()
         print(devices)
     elif args.function == "getSensors":
         sensors = tellstickHandler.getSensors()
         print(sensors)
+    elif args.function == "getGestureBoolArray":
+        boolarray = gestureTracker.getGestureBoolArray()
+        print(boolarray)
     elif args.function == "saveGesture":
         boolarray = [int(x) for x in args.args[0].strip('[]').split(',')]
         name = args.args[1]
